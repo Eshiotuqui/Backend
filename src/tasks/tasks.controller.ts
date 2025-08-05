07d83b3aa51de0844e38dada5ad3553cf52b37bd
eshiotuqui <enzoshiotuqui.dev@gmail.com>
2025-08-05 14:58:37 -0300

    BE: criando rota de post + rota com id

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post } from "@nestjs/common";
 import { TasksService } from "./tasks.service";
 
 @Controller("tasks")
@@ -6,7 +6,17 @@ export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
   @Get()
-  getTasks() {
-    return this.tasksService.listAllTasks();
+  findAllTasks() {
+    return this.tasksService.findAllTasks();
+  }
+
+  @Get(":id")
+  findTaskById(@Param("id") id: string) {
+    return this.tasksService.findTaskById(id);
+  }
+
+  @Post()
+  createTask(@Body() body: any) {
+    return this.tasksService.createTask(body);
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,7 +2,18 @@ import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class TasksService {
-  listAllTasks() {
-    return [{ id: 1, name: "Task 1", description: "Description 1" }];
+  findAllTasks() {
+    return [
+      { id: 1, name: "Task 1", description: "Description 1" },
+      { id: 2, name: "Task 2", description: "Description 2" }
+    ];
+  }
+
+  findTaskById(id: string) {
+    return { id, name: "Task 1", description: "Description 1" };
+  }
+
+  createTask(body: any) {
+    return body;
   }
 }
