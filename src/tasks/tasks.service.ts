6c4f2a67b4cb2fa1ed32cb28b6c7dce4701fb032
eshiotuqui <enzoshiotuqui.dev@gmail.com>
2025-08-06 11:06:14 -0300

    BE: listando dinamico com entity

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,19 +1,33 @@
 import { Injectable } from "@nestjs/common";
+import type { Task } from "./entities/task.entity";
 
 @Injectable()
 export class TasksService {
+  private tasks: Task[] = [
+    { id: 1, name: "Task 1", description: "Description 1", completed: false },
+    { id: 2, name: "Task 2", description: "Description 2", completed: true },
+    { id: 3, name: "Task 3", description: "Description 3", completed: true },
+    { id: 4, name: "Task 4", description: "Description 4", completed: true }
+  ];
+
   findAllTasks() {
-    return [
-      { id: 1, name: "Task 1", description: "Description 1" },
-      { id: 2, name: "Task 2", description: "Description 2" }
-    ];
+    return this.tasks;
   }
 
   findTaskById(id: string) {
-    return { id, name: "Task 1", description: "Description 1" };
+    return this.tasks.find((task) => task.id === Number(id));
   }
 
   createTask(body: any) {
-    return body;
+    const newId = this.tasks.length + 1;
+
+    const newTask = {
+      id: newId,
+      ...body
+    };
+
+    this.tasks.push(newTask);
+
+    return newTask;
   }
 }
